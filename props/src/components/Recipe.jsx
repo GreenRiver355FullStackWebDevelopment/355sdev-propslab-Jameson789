013d82a9b3893609160c666325af0f0ba00e435f
Jameson Farmer <thejamesonfarmer@gmail.com>
2025-04-15 20:30:07 -0700

    added rendering in recipe and recipeContainer

--- props/src/components/Recipe.jsx
+++ props/src/components/Recipe.jsx
@@ -1,6 +1,12 @@
-function Recipe() {
+function Recipe({ recipe }) {
+    const {name, ingredients, instructions} = recipe;
+    console.log(name);
     return (
-        <></>
+        <>
+        <h2 className="name">{name}</h2>
+        <p className="ingredients">{ingredients}</p>
+        <p className="instructions">{instructions}</p>
+        </>
     )
 }
 
--- props/src/components/RecipeContainer.jsx
+++ props/src/components/RecipeContainer.jsx
@@ -1,7 +1,18 @@
+import Recipe from "./Recipe.jsx";
+
 function RecipeContainer({ recipes }) {
-    console.log(recipes);
+    //console.log(recipes);
     return (
-       <p>hi</p>
+        <>
+        <h1 className="title">Recipes</h1>
+        <div className="recipe-container">
+        {
+            recipes.map(recipe => (
+            <Recipe recipe={recipe} key={recipe.name}></Recipe>
+            ))
+        }
+        </div>
+        </>
     )
 }
 
